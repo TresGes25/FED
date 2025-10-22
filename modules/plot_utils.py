600840a32e8e270c9afb736b2c62741081d3e9b0
Irem Sara <ir.sari@hotmail.com>
2025-10-22 14:55:20 +0100

    Added a plot

---
+++ modules/plot_utils.py
@@ -0,0 +1,14 @@
+# %%
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def make_charts(dataframe):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(data= dataframe, x='Year', y='GDP (millions USD)', hue='Country', ax = ax)
+    ax.set_xlabel('Year')
+    ax.set_ylabel('GDP (million USD)')
+    ax.set_title('GDP series')
+    ax.legend
+    plt.tight_layout()
+    plt.show()
+
