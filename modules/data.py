2caf5615428d226f16bc8207202d781f7fd21bb8
TresGes25 <107326809+TresGes25@users.noreply.github.com>
2025-11-02 13:58:12 +0000

    Removed csv from git tracking

--- modules/data.py
+++ modules/data.py
@@ -1,24 +1,21 @@
-
 import pandas as pd
 
 # add type hints AND docstrings
-def read_data(path : int) -> int:
-
+def read_data(path):
     df = pd.read_csv(path)
     return df
 
 def select_columns(df):
     df = df[["Reference area", "TIME_PERIOD", "OBS_VALUE"]]
-    df = df.rename(columns={"Reference area" : "Country",
-                    "TIME_PERIOD" : "Year",
-                    "OBS_VALUE" : "GDP (millions USD)"})
+    df = df.rename(columns={"Reference area": "Country",
+                    "TIME_PERIOD": "Year",
+                    "OBS_VALUE": "GDP (millions USD)"})
 
     df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 
     return df
 
 def missing_data(df):
-
     num_missing = df.isnull().sum().sum()
 
     if num_missing > 0:
@@ -28,7 +25,6 @@ def missing_data(df):
         return df
 
 def load_data(path):
-
     df = read_data(path)
     df = select_columns(df)
     df = missing_data(df)
